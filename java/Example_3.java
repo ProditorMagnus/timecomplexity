af1521406a2ef72aaf966cf5997b36a2c9a60e92
ProditorMagnus <public.ravana@gmail.com>
2018-03-09 19:10:21 +0200

    Start considering manual input-output configuration

---
+++ java/Example_3.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class Example_3 {
+    public static void start(long n) {
+        long c = 0;
+        for (int i = 0; i < n; i++) {
+            c++;
+        }
+    }
+}
